f29ed99d8a2fc4d86894aace51751e8dcadba175
azuy212 <ali.yasoob@discorp.com>
2021-03-22 20:32:56 +0500

    style: configure lint, commitzen and husky
    Configure husky to run lint on commit and also configure commitzen to standardize commit messages

--- hooks/useColorScheme.web.ts
+++ hooks/useColorScheme.web.ts
@@ -1,5 +1,8 @@
 // useColorScheme from react-native does not support web currently. You can replace
+
+import { ColorSchemeName } from 'react-native';
+
 // this with react-native-appearance if you would like theme support on web.
-export default function useColorScheme() {
+export default function useColorScheme(): NonNullable<ColorSchemeName> {
   return 'light';
 }
